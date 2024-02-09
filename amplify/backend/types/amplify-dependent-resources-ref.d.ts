4d45c6a93099bb9058f9cbce25375cbe1fccfe2e
Alan <alancurtis5000+accounts@gmail.com>
2024-02-09 15:36:08 -0700

    todo list working with database

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "anotes": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "anotes9b399654": {
       "AppClientID": "string",
